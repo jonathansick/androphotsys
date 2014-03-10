d0eff1658940c3bd8603a57b5a191f126c7e99b6
Jonathan Sick <jonathansick@mac.com>
2014-03-09 21:12:18 -0400

    Make a wircam_vega_to_ab function wrapper available

--- androphotsys/__init__.py
+++ androphotsys/__init__.py
@@ -1,2 +1,3 @@
 from elixir import megacam_to_sdss
 from jansky import ab_to_microjy, dn_to_microjy
+from wircam import wircam_vega_to_ab
--- androphotsys/wircam.py
+++ androphotsys/wircam.py
@@ -5,8 +5,16 @@ Convert WIRCam photometry.
 """
 
 # Constants added to vega mags to obtain AB mags
-# From Blanton et al 2005 AJ 129 2565
+# From Blanton et al 2005 AJ 129 2565 eq 5.
 WIRCAM_VEGA_TO_AB = {
     "J": 0.91,
     "H": 1.39,
     "Ks": 1.85}
+
+
+def wircam_vega_to_ab(vegamag, band):
+    """Returns the AB mag of a WIRCam AB magnitude.
+
+    Uses zeropoint transformations form Blanton et al 2005 AJ 129 2565 eq 5.
+    """
+    return vegamag + WIRCAM_VEGA_TO_AB[band]
