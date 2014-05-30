c8f0ce6a31aae25d180cc8665e9afd024dfd8bef
Jonathan Sick <jonathansick@mac.com>
2014-05-30 18:48:28 -0400

    HST colour transformations now read from table
    Tables are generated using the modelling scripts in `scripts/` and data
    tables are stored in `androphotsys/data`. Use the
    `query_colour_transformation` function to get the transformation
    coefficients.
    
    This system means less degree for error in hard coding coefficients, and
    it also becomes easier to print out a table of these coefficients too
    for publication.

--- androphotsys/hst.py
+++ androphotsys/hst.py
@@ -3,41 +3,45 @@
 """
 Photometric conversions from HST bandpasses to the Cousins system.
 
-See: Sirianni 2005.
+See: Sirianni 2005, WFC3 handbook and the FSPS colour transformation modelling
+scripts in the scripts/ directory.
 """
 
+from pkg_resources import resource_stream, resource_exists
+
 import numpy as np
+from astropy.table import Table, vstack
 
 
 WFC3_ZP = {"STMAG": {'f110w': 28.4401,
-                    'f160w': 28.1875,
-                    'f275w': 22.6034,
-                    'f336w': 23.6044},
-    "VEGAMAG": {'f110w': 26.0628,
-                'f160w': 24.6949,
-                'f275w': 22.6322,
-                'f336w': 23.4836},
-    "ABMAG": {'f110w': 26.8223,
-              'f160w': 25.9463,
-              'f275w': 24.1305,
-              'f336w': 24.6682},
-    "OBMAG": {'f110w': 0.,
-              'f160w': 0.,
-              'f275w': 0.,
-              'f336w': 0.}}
+                     'f160w': 28.1875,
+                     'f275w': 22.6034,
+                     'f336w': 23.6044},
+           "VEGAMAG": {'f110w': 26.0628,
+                       'f160w': 24.6949,
+                       'f275w': 22.6322,
+                       'f336w': 23.4836},
+           "ABMAG": {'f110w': 26.8223,
+                     'f160w': 25.9463,
+                     'f275w': 24.1305,
+                     'f336w': 24.6682},
+           "OBMAG": {'f110w': 0.,
+                     'f160w': 0.,
+                     'f275w': 0.,
+                     'f336w': 0.}}
 
 ACS_ZP = {"STMAG": {'f475w': 25.757,
                     'f606w': 26.655,
                     'f814w': 26.776},
-    "VEGAMAG": {'f475w': 26.168,
-                'f606w': 26.398,
-                'f814w': 25.501},
-    "ABMAG": {'f475w': 25.673,
-              'f606w': 26.486,
-              'f814w': 25.937},
-    "OBMAG": {'f475w': 0.,
-              'f606w': 0.,
-              'f814w': 0.}}
+          "VEGAMAG": {'f475w': 26.168,
+                      'f606w': 26.398,
+                      'f814w': 25.501},
+          "ABMAG": {'f475w': 25.673,
+                    'f606w': 26.486,
+                    'f814w': 25.937},
+          "OBMAG": {'f475w': 0.,
+                    'f606w': 0.,
+                    'f814w': 0.}}
 
 
 def transform_acs_zp(mags, band, zp_in, zp_out):
@@ -71,6 +75,7 @@ def WFC3_275_336_to_u(m275_input, e275, m336_input, e336, zp="VEGAMAG"):
     m275 = transform_wfc3_zp(m275_input, 'f275w', zp, 'ABMAG')
     m336 = transform_wfc3_zp(m336_input, 'f336w', zp, 'ABMAG')
     x = [-0.08689923, -0.35246386, 0.14181587]
+    x = query_colour_transformation("u", "F336W", ("F275W", "F336W"))
     C = m275 - m336
     return m336 + x[0] + x[1] * C + x[2] * C ** 2
 
@@ -81,8 +86,10 @@ def WFC3_110_160_to_JK(m110_input, e110, m160_input, e160, zp="VEGAMAG"):
     """
     m110 = transform_wfc3_zp(m110_input, 'f110w', zp, 'ABMAG')
     m160 = transform_wfc3_zp(m160_input, 'f160w', zp, 'ABMAG')
-    jx = [0.00547237, 0.66899301, -0.04243658]
-    kx = [0.31956416, -0.79017206, -0.50508958]
+    # jx = [0.00547237, 0.66899301, -0.04243658]
+    # kx = [0.31956416, -0.79017206, -0.50508958]
+    jx = query_colour_transformation("J", "F160W", ("F110W", "F160W"))
+    kx = query_colour_transformation("Ks", "F160W", ("F110W", "F160W"))
     C = m110 - m160
     J = m160 + jx[0] + jx[1] * C + jx[2] * C ** 2
     K = m160 + kx[0] + kx[1] * C + kx[2] * C ** 2
@@ -95,9 +102,12 @@ def ACS_475_814_to_gri(m475_input, e475, m814_input, e814, zp="VEGAMAG"):
     """
     m475 = transform_acs_zp(m475_input, 'f475w', zp, 'ABMAG')
     m814 = transform_acs_zp(m814_input, 'f814w', zp, 'ABMAG')
-    gx = [0.00011405, 0.01957309, 0.00617221]
-    rx = [0.01195939, 0.4330676, -0.07053753]
-    ix = [0.00137411, 0.10460093, -0.01623761]
+    # gx = [0.00011405, 0.01957309, 0.00617221]
+    # rx = [0.01195939, 0.4330676, -0.07053753]
+    # ix = [0.00137411, 0.10460093, -0.01623761]
+    gx = query_colour_transformation("g", "F475W", ("F475W", "F814W"))
+    rx = query_colour_transformation("r", "F814W", ("F475W", "F814W"))
+    ix = query_colour_transformation("i", "F814W", ("F475W", "F814W"))
     C = m475 - m814
     g = m475 + gx[0] + gx[1] * C + gx[2] * C ** 2
     r = m814 + rx[0] + rx[1] * C + rx[2] * C ** 2
@@ -111,14 +121,51 @@ def ACS_606_814_to_gri(m606_input, e606, m814_input, e814, zp="STMAG"):
     """
     m606 = transform_acs_zp(m606_input, 'f606w', zp, 'ABMAG')
     m814 = transform_acs_zp(m814_input, 'f814w', zp, 'ABMAG')
-    gx = [-0.01747885, 0.92625851, 0.48930022]
-    rx = [0.00288843, 0.79799345, -0.02999439]
-    ix = [-0.00116191, 0.19501024, -0.00117196]
+    # gx = [-0.01747885, 0.92625851, 0.48930022]
+    # rx = [0.00288843, 0.79799345, -0.02999439]
+    # ix = [-0.00116191, 0.19501024, -0.00117196]
+    gx = query_colour_transformation("g", "F606W", ("F606W", "F814W"))
+    rx = query_colour_transformation("r", "F814W", ("F606W", "F814W"))
+    ix = query_colour_transformation("i", "F814W", ("F606W", "F814W"))
     C = m606 - m814
     g = m606 + gx[0] + gx[1] * C + gx[2] * C ** 2
     r = m814 + rx[0] + rx[1] * C + rx[2] * C ** 2
     i = m814 + ix[0] + ix[1] * C + ix[2] * C ** 2
     return g, r, i
+
+
+def query_colour_transformation(tmag, smag, colour):
+    """Get the colour transformation for the target bandpass `tmag`
+    given a source bandpass `smag` and colour `colour`. Colour transformation
+    coefficients are s.t.
+
+    tmag = smag + x[0] + x[1] * colour + x[2] * colour^2
+    """
+    tbl = load_colour_transformation_table()
+    i = np.where((tbl['TMAG'] == tmag) &
+                 (tbl['SMAG'] == smag) &
+                 (tbl['COL'] == "-".join(colour)))[0][0]
+    return (tbl['x0'][i], tbl['x1'][i], tbl['x2'][i])
+
+
+CT_TABLE = None
+
+
+def load_colour_transformation_table():
+    global CT_TABLE
+    if CT_TABLE is None:
+        table_names = ['wfc3_u', 'phat_acs', 'acs_brown', 'wfc3_wircam']
+        tables = [Table.read(_read_colour_transformation_table(n),
+                             format='ascii.commented_header')
+                  for n in table_names]
+        CT_TABLE = vstack(tables)
+    return CT_TABLE
+
+
+def _read_colour_transformation_table(name):
+    path = "data/{0}.txt".format(name)
+    assert resource_exists(__name__, path)
+    return resource_stream(__name__, path)
 
 
 def ACS_606_814_to_VRI(m606_input, e606, m814_input, e814, zp="STMAG"):
