a921eeb2cdd4bce9ac7d691355a960bbbb002063
Jonathan Sick <jonathansick@mac.com>
2014-10-16 13:39:24 -0400

    Add sdss_to_megacam photometric conversions

--- androphotsys/__init__.py
+++ androphotsys/__init__.py
@@ -1,4 +1,4 @@
-from .elixir import megacam_to_sdss
+from .elixir import megacam_to_sdss, sdss_to_megacam
 from .jansky import ab_to_microjy, dn_to_microjy, microjy_to_ab
 from .msun import solar_mag
 from .wircam import wircam_vega_to_ab
--- androphotsys/elixir.py
+++ androphotsys/elixir.py
@@ -8,8 +8,8 @@ Photometric system conversions for MegaCam/Elixir-LSB data.
 
 
 def megacam_to_sdss(u=None, g=None, r=None, i=None,
-        u_err=None, g_err=None, r_err=None, i_err=None,
-        XU=0.241, XG=0.153, XR=0.024, XI=0.003):
+                    u_err=None, g_err=None, r_err=None, i_err=None,
+                    XU=0.241, XG=0.153, XR=0.024, XI=0.003):
     """Convert MegaCam magnitudes to SDSS magnitudes (both in AB system)."""
 
     if g is not None:
@@ -41,3 +41,28 @@ def megacam_to_sdss(u=None, g=None, r=None, i=None,
         i_sdss = None
 
     return {"u": u_sdss, "g": g_sdss, "r": r_sdss, "i": i_sdss}
+
+
+def sdss_to_megacam(u=None, g=None, r=None, i=None):
+    """Convert a SDSS magnitude to the megacam system
+
+    http://www3.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/en/megapipe/docs/filt.html
+    """
+    if u is not None and g is not None:
+        u_mega = u - 0.241 (u - g)
+    else:
+        u_mega = None
+
+    if g is not None and r is not None:
+        g_mega = g - 0.153 (g - r)
+        r_mega = r - 0.024 (g - r)
+    else:
+        g_mega = None
+        r_mega = None
+
+    if r is not None and i is not None:
+        i_mega = i - 0.003 (r - i)  # (new filter)
+    else:
+        i_mega = None
+
+    return {"u": u_mega, "g": g_mega, "r": r_mega, "i": i_mega}
