837a33a6b6a7365b94b7b21022ba89f620102538
Jonathan Sick <jonathansick@mac.com>
2014-10-22 14:21:49 -0400

    Add translation for phat filter names

--- androphotsys/fspsfilters.py
+++ androphotsys/fspsfilters.py
@@ -14,7 +14,11 @@ ANDROIDS_TO_FSPS = {'u': 'megacam_u',
                     '36': 'irac_1',
                     '45': 'irac_2',
                     '58': 'irac_3',
-                    '80': 'irac_4'}
+                    '80': 'irac_4',
+                    'f475w': 'wfc_acs_f475w',
+                    'f814w': 'wfc_acs_f814w',
+                    'f110w': 'wfc3_ir_f110w',
+                    'f160w': 'wfc3_ir_f160w'}
 FSPS_TO_ANDROIDS = {v: k for k, v in ANDROIDS_TO_FSPS.iteritems()}
 
 
