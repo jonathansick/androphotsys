a7d1075e2c2cd8857cc115390f7bd5b0d7b56165
Jonathan Sick <jonathansick@mac.com>
2014-12-22 07:15:40 -0500

    All flux conversions consolidated to sedbot

--- androphotsys/__init__.py
+++ androphotsys/__init__.py
@@ -1,10 +1,9 @@
-from .elixir import megacam_to_sdss, sdss_to_megacam
-from .jansky import ab_to_microjy, dn_to_microjy, microjy_to_ab
-from .msun import solar_mag
-from .wircam import wircam_vega_to_ab
-from .hst_cousins import ACS_606_814_to_VRI, ACS_475_814_to_BVRI
-from .hst import WFC3_275_336_to_u, WFC3_110_160_to_JK, ACS_475_814_to_gri
-from .hst import ACS_606_814_to_gri
-from .cousins import VRI_to_gri, BVRI_to_ugri, FIG96_UBVRc_to_ugr
-from .fspsfilters import fsps_name_to_androids, androids_name_to_fsps
-from .fspsfilters import latex_name
+from .elixir import megacam_to_sdss, sdss_to_megacam  # NOQA
+from .msun import solar_mag  # NOQA
+from .wircam import wircam_vega_to_ab  # NOQA
+from .hst_cousins import ACS_606_814_to_VRI, ACS_475_814_to_BVRI  # NOQA
+from .hst import WFC3_275_336_to_u, WFC3_110_160_to_JK, ACS_475_814_to_gri  # NOQA
+from .hst import ACS_606_814_to_gri  # NOQA
+from .cousins import VRI_to_gri, BVRI_to_ugri, FIG96_UBVRc_to_ugr  # NOQA
+from .fspsfilters import fsps_name_to_androids, androids_name_to_fsps  # NOQA
+from .fspsfilters import latex_name  # NOQA
